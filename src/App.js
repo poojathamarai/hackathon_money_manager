ef1dadc6604be16adfb4bd5d30dc2cb779ff21ed
Pooja <92292344+poojathamarai@users.noreply.github.com>
2022-01-19 22:20:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,13 @@ import {Switch,Route} from "react-router-dom";
 import { DisplayExpense } from './Display_expense';
 import { NotFound } from './NotFound';
 import { DisplayIncome } from './Display_income';
-import { useState,useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import { AddIncome } from './Add_income';
 import { AddExpense } from './Add_expense';
 import Button from '@mui/material/Button';
 import { Home } from './Home';
 function App() {
-  const [incomelist,setIncomeList]=useState();
-  const [expenselist,setExpenseList]=useState();
   const history = useHistory();
- 
-  useEffect(()=>{
-    fetch("https://61e81129e32cd90017acbfe1.mockapi.io/income").then((data)=>data.json()).then((obj)=>setIncomeList(obj))
-  },[]) 
-  useEffect(()=>{
-    fetch("https://61e81129e32cd90017acbfe1.mockapi.io/expense").then((data)=>data.json()).then((obj)=>setExpenseList(obj))
-  },[]) 
   return ( 
   
   <div className="App">
